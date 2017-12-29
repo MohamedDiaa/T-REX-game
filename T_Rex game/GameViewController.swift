fa6c363206a6fa0fe8c6e8e0e07ab2b8c750b8c7
Diaa Alwakil <diaa.alwakil@svea.com>
2017-12-29 16:48:36 +0100

    Removed Unnecessary file template. Added Images for Monster Ready,Set and Go.

--- T_Rex game/GameViewController.swift
+++ T_Rex game/GameViewController.swift
@@ -17,13 +17,11 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            let scene = GameScene()
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            scene.scaleMode = .resizeFill
+            // Present the scene
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
             
