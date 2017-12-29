149052cf8d489a269756e4898c19510fe44f6d7b
Diaa Alwakil <diaa.alwakil@svea.com>
2017-12-29 17:12:27 +0100

    Taught the monster How to walk in a wired way.

--- T_Rex game/GameScene.swift
+++ T_Rex game/GameScene.swift
@@ -12,13 +12,36 @@ import GameplayKit
 class GameScene: SKScene {
     
     
-    override func didMove(to view: SKView) {
+    let ready = SKTexture(image: #imageLiteral(resourceName: "Ready"))
+    let set = SKTexture(image: #imageLiteral(resourceName: "Set"))
+    let go = SKTexture(image: #imageLiteral(resourceName: "Go"))
+
+    let monster = SKSpriteNode(texture:  SKTexture(image: #imageLiteral(resourceName: "Ready")))
+    
+    override func didMove(to view: SKView) 
+    {
         
         backgroundColor = SKColor.white
         
-        let monster = SKSpriteNode(imageNamed: "Ready")
         monster.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
         self.addChild(monster)
 
+       let action = SKAction.repeatForever(SKAction.sequence([
+            
+       SKAction.wait(forDuration: 0.1),
+        SKAction.run {
+            self.monster.texture = self.ready
+        },
+       SKAction.wait(forDuration: 0.1),
+        SKAction.run {
+            self.monster.texture = self.go
+        },
+        SKAction.wait(forDuration: 0.1),
+        SKAction.run {
+            self.monster.texture = self.set
+        }
+    ]))
+  
+       self.monster.run(action)
     }
 }
